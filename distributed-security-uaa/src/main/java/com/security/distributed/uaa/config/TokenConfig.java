41a37db7ae5ef0cebc63cba2c48f8124804e4140
Administrator <dymdoudou@126.com>
2021-03-22 14:59:56 +0800

    Initial commit

---
+++ distributed-security-uaa/src/main/java/com/security/distributed/uaa/config/TokenConfig.java
@@ -0,0 +1,45 @@
+package com.security.distributed.uaa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@Configuration
+public class TokenConfig {
+
+    /*private String SIGNING_KEY = "uaa123";
+
+    @Bean
+    public TokenStore tokenStore() {
+        //JWT令牌存储方案
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY); //对称秘钥，资源服务器使用该秘钥来验证
+        return converter;
+    }*/
+
+    @Bean
+    public TokenStore tokenStore() { return new InMemoryTokenStore(); }
+//    @Autowired
+//    DataSource dataSource;
+//
+//    @Bean
+//    public TokenStore tokenStore() {
+//        return new JdbcTokenStore(dataSource);
+//    }
+}
---
+++ distributed-security-uaa/src/main/java/com/security/distributed/uaa/service/SpringDataUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.security.distributed.uaa.service;
+
+import com.security.distributed.uaa.dao.UserDao;
+import com.security.distributed.uaa.model.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@Service
+public class SpringDataUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserDao userDao;
+
+    //根据 账号查询用户信息
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //将来连接数据库根据账号查询用户信息
+        UserDto userDto = userDao.getUserByUsername(username);
+        if(userDto == null){
+            //如果用户查不到，返回null，由provider来抛出异常
+            return null;
+        }
+        //根据用户的id查询用户的权限
+        List<String> permissions = userDao.findPermissionsByUserId(userDto.getId());
+        //将permissions转成数组
+        String[] permissionArray = new String[permissions.size()];
+        permissions.toArray(permissionArray);
+        //将用户名称、用户密码以及用户用户拥有权限放入UserDetails中
+        UserDetails userDetails = User.withUsername(userDto.getUsername()).password(userDto.getPassword()).authorities(permissionArray).build();
+        //将userDto转成json
+//        String principal = JSON.toJSONString(userDto);
+        //扩展用户信息将用户实体转换成json存入userDetails中
+        //UserDetails userDetails = User.withUsername(principal).password(userDto.getPassword()).authorities(permissionArray).build();
+        return userDetails;
+    }
+}
